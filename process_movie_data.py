09b5f77355775db0e98c2a19b4287cdad371f748
Ethan Vazquez <edv121@outlook.com>
2023-10-25 22:56:26 -0700

    top 10

--- process_movie_data.py
+++ process_movie_data.py
@@ -35,10 +35,8 @@ def find_top_N(filename, n):
 # Movie data comes from "Movie Gross and Ratings" dataset on Kaggle by Yashwanth Sharaf
 # https://www.kaggle.com/datasets/thedevastator/movie-gross-and-ratings-from-1989-to-2014
 if __name__ == "__main__":
-    n = 5
-
     if len(sys.argv) < 2:
-        n = 5
+        n = 10
     else:
         n = int(sys.argv[1].split("=")[1])
 
